eda49749bd4e6f22bf6ec4da57644eb78f705a6e
Your Name <you@example.com>
2024-08-31 04:31:35 -0400

    Task 5 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,8 +50,11 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  if (isbn in books) {
+    return res.status(200).json(books[isbn].reviews);
+  }
+  return res.status(404).json({message: "ISBN not found"});
 });
 
 module.exports.general = public_users;
