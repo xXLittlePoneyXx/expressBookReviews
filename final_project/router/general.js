a598e9a4f4e3156e4fbc07d26d305d1a01608600
Your Name <you@example.com>
2024-08-31 06:07:33 -0400

    Task 10 to 13 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,40 +25,68 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(200).json(JSON.stringify(books));
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved")
+        },6000)})
+    
+    myPromise.then((successMessage) => {
+        return res.status(200).json(JSON.stringify(books));
+    })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  let isbn = req.params.isbn;
-  if (isbn in books) {
-    return res.status(200).json(books[isbn]);
-  }
-  return res.status(404).json({message: "ISBN not found"});
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved")
+        },6000)})
+    
+    myPromise.then((successMessage) => {
+        let isbn = req.params.isbn;
+        if (isbn in books) {
+            return res.status(200).json(books[isbn]);
+        }
+        return res.status(404).json({message: "ISBN not found"});
+    })
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  let author = req.params.author;
-  Object.keys(books).forEach(function(key, value){
-    console.log(books[key]);
-    if (books[key].author === author) {
-        return res.status(200).json(books[key]);
-    }
-  });
-  return res.status(404).json({message: "Author not found"});
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved")
+        },6000)})
+    
+    myPromise.then((successMessage) => {
+        let author = req.params.author;
+        Object.keys(books).forEach(function(key, value){
+            console.log(books[key]);
+            if (books[key].author === author) {
+                return res.status(200).json(books[key]);
+            }
+        });
+        return res.status(404).json({message: "Author not found"});
+    })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  let title = req.params.title;
-  Object.keys(books).forEach(function(key, value){
-    console.log(books[key]);
-    if (books[key].title === title) {
-        return res.status(200).json(books[key]);
-    }
-  });
-  return res.status(404).json({message: "Title not found"});
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved")
+        },6000)})
+    
+    myPromise.then((successMessage) => {
+        let title = req.params.title;
+        Object.keys(books).forEach(function(key, value){
+            console.log(books[key]);
+            if (books[key].title === title) {
+                return res.status(200).json(books[key]);
+            }
+        });
+        return res.status(404).json({message: "Title not found"});
+    })
 });
 
 //  Get book review
