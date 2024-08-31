062c3930f270f9c280be8767c15af0ded2aa30f7
Your Name <you@example.com>
2024-08-31 04:26:28 -0400

    Task 4 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,8 +38,14 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  Object.keys(books).forEach(function(key, value){
+    console.log(books[key]);
+    if (books[key].title === title) {
+        return res.status(200).json(books[key]);
+    }
+  });
+  return res.status(404).json({message: "Title not found"});
 });
 
 //  Get book review
